bdbf508ff2422d5198e8bb76635a3c909e7966e4
Bruce Campbell <campbellbd@ldschurch.org>
2017-01-09 17:00:07 -0700

    Wire BookSearch and BookList into Home

--- src/app/components/Home/Home.js
+++ src/app/components/Home/Home.js
@@ -1,11 +1,19 @@
 import Greeting from '../Greeting';
 import Recommendations from "../Recommendations";
+import BookSearch from "../BookSearch";
+import BookList from "../BookList";
 
 const Home = ({greet = true, greetingAction, name = "World", recommendations}) => (
     <div>
         {greet ? <Greeting name={name} close={greetingAction} /> : null}
 
         <Recommendations books={recommendations} />
+        <BookSearch />
+        <BookList zebra title="Reading List">
+            <p>hey</p>
+            <p>ho</p>
+            <p>let's go</p>
+        </BookList>
 
     </div>
 );
